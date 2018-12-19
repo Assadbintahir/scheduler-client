b8f1fe1be5e6a6752a9e31710815360db180ec6b
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-12-20 00:52:10 +0500

    Added actions test files

---
+++ client/src/actions/__tests__/auth.test.js
@@ -0,0 +1,4 @@
+describe('auth actions', () => {
+  it('should handle LoginAction');
+  it('should handle LogoutAction');
+});
---
+++ client/src/actions/__tests__/job.test.js
@@ -0,0 +1,7 @@
+describe('jobs actions', () => {
+  it('should handle FetchJobsAction');
+  it('should handle CreateJobAction');
+  it('should handle DeleteJobAction');
+  it('should handle DisableJobAction');
+  it('should handle EnableJobAction');
+});
