faf50589f14b3d73082d41d7a5aab8480ae25001
dayem10pearls <dayem.siddiqui@tenpearls.com>
2019-01-01 12:05:05 +0500

    Added a sample test

--- client/src/actions/__tests__/auth.test.js
+++ client/src/actions/__tests__/auth.test.js
@@ -1,4 +1,12 @@
+import { LoginAction, LogoutAction } from '../auth.actions';
+import * as ActionTypes from '../auth.actions';
+
 describe('auth actions', () => {
-  it('should handle LoginAction');
+  it('should handle LoginAction', () => {
+    const LoginActionOutput = {
+      type: ActionTypes.LOGIN_USER,
+      payload: {},
+    };
+  });
   it('should handle LogoutAction');
 });
